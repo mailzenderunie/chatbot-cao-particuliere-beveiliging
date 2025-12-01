ce1c91b15cf21b44721e4d0e43adabfa2d88cf2c
mailzenderunie <mailzenderunie@gmail.com>
2025-12-01 15:13:46 +0100

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import Script from "next/script";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "AgentKit demo",
+  description: "Demo of ChatKit with hosted workflow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bitter:wght@600&family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+
+        <Script
+          src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
---
+++ lib/config.ts
@@ -0,0 +1,43 @@
+import { ColorScheme, StartScreenPrompt, ThemeOption } from "@openai/chatkit";
+
+export const WORKFLOW_ID =
+  process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID?.trim() ?? "";
+
+export const CREATE_SESSION_ENDPOINT = "/api/create-session";
+
+export const STARTER_PROMPTS: StartScreenPrompt[] = [
+  {
+    label: "Hoeveel vakantiedagen heb ik?",
+    prompt: "Hoeveel vakantiedagen heb ik?",
+    icon: "circle-question",
+  },
+  {
+    label: "Ik heb een vraag over ziekte of verlof",
+    prompt: "Ik heb een vraag over ziekte of verlof",
+    icon: "circle-question",
+  },
+];
+
+
+export const PLACEHOLDER_INPUT = "Stel hier je vraag over de cao Contractcatering...";
+
+export const GREETING = "Hoi, ik ben Una! Waarmee kan ik je helpen?";
+
+export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
+  color: {
+    grayscale: {
+      hue: 220,
+      tint: 6,
+      shade: theme === "dark" ? -1 : -4,
+    },
+accent: {
+  primary: theme === "dark" ? "#f1f5f9" : "#0f172a",
+  level: 1,
+},
+
+
+  },
+  radius: "round",
+  // Add other theme options here
+  // chatkit.studio/playground to explore config options
+});
