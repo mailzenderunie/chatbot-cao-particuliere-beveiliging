90d6138e02d51407ed4b766c10a751ac485ad827
De Unie <mailzenderunie@gmail.com>
2026-01-07 13:21:56 +0100

    Update page.tsx
    Terug naar chatkit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import SimpleChat from "@/components/SimpleChat";
+import App from "./App";
 
 export default function Home() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-semibold mb-4">
-        Chat over de cao Particuliere Beveiliging
-      </h1>
-      <SimpleChat />
-    </main>
-  );
+  return <App />;
 }
